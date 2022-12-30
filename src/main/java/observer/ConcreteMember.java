79f6ce612095bcc76a3f3b2075ba8d3bd86901ee
YoadTamar <yoad.tamar123@gmail.com>
2022-12-30 22:30:43 +0200

    add documentation to GroupAdmin and ConcreteMember

--- src/main/java/observer/ConcreteMember.java
+++ src/main/java/observer/ConcreteMember.java
@@ -1,22 +1,44 @@
 package observer;
 
+/**
+ * This class implements the Member interface in the observer design pattern.
+ * This class has 2 fields: "usb" to hold the updated UndoableStringBuilder, and a "memberName".
+ *
+ * @author Yoad and Ron
+ */
 public class ConcreteMember implements Member{
 
     String memberName ="";
     UndoableStringBuilder usb;
 
+    /**
+     * A constructor that create a new object of type ConcreteMember
+     *
+     * @param name - the name of tne new member
+     */
     public ConcreteMember(String name)
     {
         memberName = name;
         usb=new UndoableStringBuilder();
     }
 
+    /**
+     *The method update the usb
+     *The copy make by a shallow copy
+     *
+     * @param usb - the new UndoableStringBuilder that usb need to point to
+     */
     @Override
     public void update(UndoableStringBuilder usb) {
         System.out.println(memberName + " is notified");
         this.usb=usb;
     }
 
+    /**
+     *the method return a string that hold the member's name and the UndoableStringBuilder
+     *
+     * @return memberName
+     */
     public String toString(){
         return memberName + ": " +  this.usb.toString();
     }
--- src/main/java/observer/GroupAdmin.java
+++ src/main/java/observer/GroupAdmin.java
@@ -3,16 +3,35 @@ package observer;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class implements the Sender interface in the observer design pattern.
+ * This class has 2 fields:
+ * "usb" to hold the main UndoableStringBuilder object - the most update one.
+ * Member type list "members" to keep all members.
+ *
+ * @author Yoad and ron
+ */
 public class GroupAdmin implements Sender{
 
     UndoableStringBuilder usb;
     List<Member> members;
 
-        public GroupAdmin(){
+    /**
+     * A constructor that create a new object of type GroupAdmin.
+     */
+    public GroupAdmin(){
         this.members=new ArrayList<>();
         this.usb= new UndoableStringBuilder();
     }
 
+    /**
+     *
+     *The method add a new member to the member list.
+     *It can not accept a null member, or it won't add a member that already add.
+     *
+     * @param obj - a new member.
+     * @throws NullPointerException - if the user try to add a null member - throw an exception.
+     */
     @Override
     public void register(Member obj) throws NullPointerException
     {
@@ -30,6 +49,14 @@ public class GroupAdmin implements Sender{
         }
     }
 
+    /**
+     * The method remove a member from the member list.
+     * It can not receive a null member to remove.
+     * If the member is not exist in the list - the method don't change a thing.
+     *
+     * @param obj - the member we need to remove
+     * @throws NullPointerException -  if the user try to remove a null member - throw an exception.
+     */
     @Override
     public void unregister(Member obj) throws NullPointerException  {
         if (obj==null){
@@ -44,30 +71,57 @@ public class GroupAdmin implements Sender{
         }
     }
 
+    /**
+     * The method use the insert method of the UndoableStringBuilder class.
+     * Then, notify all the members in the list.
+     *
+     * @param offset - the start position to insert
+     * @param obj -  the string to insert
+     */
     @Override
     public void insert(int offset, String obj) {
         usb.insert(offset,obj);
         notifyObservers();
     }
 
+    /**
+     * The method use the append method of the UndoableStringBuilder class.
+     * Then, notify all the members in the list.
+     *
+     * @param obj - the string we want to append
+     */
     @Override
     public void append(String obj) {
         usb.append(obj);
         notifyObservers();
     }
 
+    /**
+     * The method use the delete method of the UndoableStringBuilder class.
+     * Then, notify all the members in the list.
+     *
+     * @param start - the start position of delete
+     * @param end - the end position of delete
+     */
     @Override
     public void delete(int start, int end) {
         usb.delete(start, end);
         notifyObservers();
     }
 
+    /**
+     * The method use the undo method of the UndoableStringBuilder class.
+     * Then, notify all the members in the list.
+     */
     @Override
     public void undo() {
         usb.undo();
         notifyObservers();
     }
 
+    /**
+     * The method notify and update all the members UndoableStringBuilder to a new UndoableStringBuilder pointer
+     */
     public void notifyObservers(){
         for (Member member: members){
             member.update(this.usb);
@@ -75,6 +129,10 @@ public class GroupAdmin implements Sender{
         System.out.println("all the members are notified");
     }
 
+    /**
+     *the method return a string that hold all the member's names and the UndoableStringBuilders
+     * @return String
+     */
     @Override
     public String toString() {
         String str = "Members:\n";
